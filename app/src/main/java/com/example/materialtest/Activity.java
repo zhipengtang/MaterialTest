47b90c05626f0f16c795ff7588b69b09d229f8b4
2450677610@qq.com <2450677610@qq.com>
2017-09-11 15:10:21 +0800

    commit

--- app/src/main/java/com/example/materialtest/Activity.java
+++ app/src/main/java/com/example/materialtest/Activity.java
@@ -2,12 +2,14 @@ package com.example.materialtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Activity extends AppCompatActivity {
-
+    private static final String TAG = "Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
+        Log.d(TAG, "onCreate: ");
     }
 }
